163578278966bb30f33e6b98022dc6ea4df8c661
DPOH-VAR <dpohvar@gmail.com>
2022-05-26 02:27:18 +0500

    update static paths

--- src/static/static.requests.ts
+++ src/static/static.requests.ts
@@ -3,34 +3,34 @@ import path from "path";
 
 export const staticRouter: Router = Router();
 
-const indexHtmlPath = path.resolve('./dist/web/index.html');
+const indexHtmlPath = path.resolve(__dirname+'/web/index.html');
 staticRouter.get('/', (req, res) => {
     res.setHeader("X-Frame-Options", "DENY");
     res.sendFile(indexHtmlPath);
 })
 
-const roomCreateHtmlPath = path.resolve('./dist/web/roomCreate.html');
+const roomCreateHtmlPath = path.resolve(__dirname+'/web/roomCreate.html');
 staticRouter.get('/room/create', (req, res) => {
     res.setHeader("X-Frame-Options", "DENY");
     res.sendFile(roomCreateHtmlPath);
 });
 
-const roomConnectHtmlPath = path.resolve('./dist/web/connect.html');
+const roomConnectHtmlPath = path.resolve(__dirname+'/web/connect.html');
 staticRouter.get('/room/connect', (req, res) => {
     res.sendFile(roomConnectHtmlPath);
 });
 
-const roomJoinHtmlPath = path.resolve('./dist/web/roomJoin.html');
+const roomJoinHtmlPath = path.resolve(__dirname+'/web/roomJoin.html');
 staticRouter.get('/room/:id', (req, res) => {
     res.setHeader("X-Frame-Options", "DENY");
     res.sendFile(roomJoinHtmlPath);
 });
 
-staticRouter.use('/assets', express.static('./dist/web/assets'));
-staticRouter.use('/css', express.static('./dist/web/css'));
-staticRouter.use('/style', express.static('./dist/web/style'));
-staticRouter.use('/webfonts', express.static('./dist/web/webfonts'));
-staticRouter.use('/connect.js', express.static('./dist/web/connect.js'));
-staticRouter.use('/index.js', express.static('./dist/web/index.js'));
-staticRouter.use('/roomCreate.js', express.static('./dist/web/roomCreate.js'));
-staticRouter.use('/roomJoin.js', express.static('./dist/web/roomJoin.js'));
+staticRouter.use('/assets', express.static(__dirname+'/web/assets'));
+staticRouter.use('/css', express.static(__dirname+'/web/css'));
+staticRouter.use('/style', express.static(__dirname+'/web/style'));
+staticRouter.use('/webfonts', express.static(__dirname+'/web/webfonts'));
+staticRouter.use('/connect.js', express.static(__dirname+'/web/connect.js'));
+staticRouter.use('/index.js', express.static(__dirname+'/web/index.js'));
+staticRouter.use('/roomCreate.js', express.static(__dirname+'/web/roomCreate.js'));
+staticRouter.use('/roomJoin.js', express.static(__dirname+'/web/roomJoin.js'));
